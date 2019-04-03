359549abbddf84f356af22f13eefaa3508aa13b7
junjo255 <jun.jo249@gmail.com>
2019-04-03 19:14:29 -0400

    First heroku deploy

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+// const db = require('../database');
+const path = require("path");
+const cors = require("cors");
+const port = process.env.PORT || 3000
+
+
+app.use(cors());
+app.use(parser.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname + "/../dist")));
+
+
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"));
+  });
+
+
+app.listen(port, () => {
+    console.log(`connected to the server:${port}`);
+});
+
+
