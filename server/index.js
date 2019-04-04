b7764b22aab6a4a625083cebffd18a99b3416e66
junjo255 <jun.jo249@gmail.com>
2019-04-04 15:50:50 -0400

    implemented @media css

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const parser = require("body-parser");
 // const db = require('../database');
 const path = require("path");
 const cors = require("cors");
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 
 app.use(cors());
