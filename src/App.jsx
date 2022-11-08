5503115b0cb42e072d9d0d81f93d87c5440d4b08
Eric Jo <43450544+junjo255@users.noreply.github.com>
2022-11-08 10:31:16 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import LogoPage from './components/LogoPage.jsx'
 import Countdown from './components/Countdown.jsx'
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
-        
-        <LogoPage />
+    render() {
+        return (
+            <div id="mainContainer">
 
-      <div className="wrapper">
-          <ul>
-              <li className="facebook"><a href="#"><i className="fa fa-facebook fa-2x" aria-hidden="true"/></a>
-              </li>
-              <li className="twitter"><a href="#"><i className="fa fa-twitter fa-2x" aria-hidden="true"/></a>
-              </li>
-              <li className="instagram"><a href="#"><i className="fa fa-instagram fa-2x" aria-hidden="true"/></a>
-              </li>
-              <li className="google"><a href="#"><i className="fa fa-google fa-2x" aria-hidden="true"/></a></li>
-              <li className="whatsapp"><a href="#"><i className="fa fa-whatsapp fa-2x" aria-hidden="true"/></a>
-              </li>
-          </ul>
-      </div>
-        <div className="phraseWeb">2022 FIFA World Cup</div>
-        
-        <div className="phraseSoon">starts in</div>
-        <Countdown date={`2022-11-21T00:00:00`} />
-      </div>
-    )
-  }
+                <div className="container">
+                    <LogoPage/>
+                </div>
+
+                <div className="container">
+                    <div className="wrapper">
+                        <ul>
+                            <li className="linkedin">
+                                <a href="#">
+                                    <i
+                                        className="fa fa-linkedin fa-2x"
+                                        aria-hidden="true"/>
+                                </a>
+                            </li>
+                            <li className="github">
+                                <a href="#">
+                                    <i
+                                        className="fa fa-github fa-2x"
+                                        aria-hidden="true"/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="phraseWeb">2022 FIFA World Cup</div>
+
+                    <div className="phraseSoon">starts in</div>
+                    <Countdown date={`2022-11-21T00:00:00`}/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App
